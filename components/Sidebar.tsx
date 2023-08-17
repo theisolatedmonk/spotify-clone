9777f3bd4b2263c4d41a6ff7d5eec509f00b91ab
theisolatedmonk <96rahulseth@gmail.com>
2023-08-17 22:56:31 +0530

    .

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,12 +3,14 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 interface SidebarProps {
   children: React.ReactNode;
 }
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   const pathname = usePathname();
-  const routs = useMemo(
+  const routes = useMemo(
     () => [
       {
         icon: HiHome,
@@ -37,7 +39,14 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
     w-[300px]
     p-2"
       >
-        <Box>Sidebar Navigation</Box>
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SidebarItem lable={""} key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">Song Library</Box>
       </div>
     </div>
   );
---
+++ components/SidebarItem.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { IconType } from "react-icons";
+import { twMerge } from "tailwind-merge";
+interface SidebarItemProps {
+  icon: IconType;
+  lable: string;
+  active: boolean;
+  href: string;
+}
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  icon: Icon,
+  lable,
+  active,
+  href,
+}) => {
+  return (
+    <Link
+      href={href}
+      className={twMerge(
+        `flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer
+        hover:text-white transit  text-neutral-400
+        py-1`,
+        active && "text-white"
+      )}
+    >
+      <Icon size={26} />
+      <p className="truncate w-full">{lable}</p>
+    </Link>
+  );
+};
+export default SidebarItem;
